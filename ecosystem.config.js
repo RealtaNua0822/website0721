0b2cd97c5e80df82be2ed5668f9f7494f677ae85
realtanua <yankun924@gmail.com>
2025-11-07 09:57:16 +0800

    PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'website0721-backend',
+    script: './backend/server.js',
+    instances: 1,
+    exec_mode: 'fork',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    },
+    error_file: '/var/www/website0721/logs/err.log',
+    out_file: '/var/www/website0721/logs/out.log',
+    log_file: '/var/www/website0721/logs/combined.log',
+    time: true,
+    cwd: '/var/www/website0721/backend'
+  }]
+};
